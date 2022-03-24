bc10f4089fc28fb7b90d1bdbf2deb4946fbacfb8
schillerandrew <hyogoetophile@gmail.com>
2022-03-24 13:40:20 -0700

    finished 1st TODO in catalog.js: creates dropdown menu with items in it

--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    let optionElement = document.createElement('option');
+    optionElement.textContent = Product.allProducts[i].name;
+    optionElement.value = Product.allProducts[i].name;
+    selectElement.appendChild(optionElement);
   }
 
 }
